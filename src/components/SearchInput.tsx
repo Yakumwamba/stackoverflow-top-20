ba5ee78042e7cc5f7dab0b4edace7214abaa9da1
Yakumwamba <brianlemba@gmail.com>
2023-05-27 19:22:30 +0200

    UI: updated SearchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -9,7 +9,7 @@ const SearchInput = ({ value, onChange }: SearchInputProps) => {
   return (
     <div className="flex   gap-2 flex-row bg-black  w-full items-center">
       <input
-        className="flex h-20 sm:scale-75 sm:w-full text-black sm:text-2xl lg:text-4xl w-full py-5 lg:px-10 my-10 placeholder:text-3xl placeholder:text-center"
+        className="flex h-20 p-5 sm:scale-75 sm:w-full text-black sm:text-2xl lg:text-4xl w-full py-5 lg:px-10 my-10 placeholder:text-3xl placeholder:text-center"
         type="text"
         value={value}
         placeholder="Search / Filter"
